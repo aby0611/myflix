a00247ccad74280279ddecdec92c71510ef83936
Steven Huang <stevenhuang42195@gmail.com>
2016-07-06 21:19:12 +0800

    1-9 Assignment: Test Validations

--- spec/models/video_spec.rb
+++ spec/models/video_spec.rb
@@ -6,4 +6,16 @@ describe Video do
     video.save
     expect(Video.first).to eq(video)
   end
+
+  it "does not save a video without a title" do
+    video = Video.create(description: "a great video")
+    video.save
+    expect(Video.count).to eq(0)
+  end
+
+  it "does not save a video wothout a description" do
+    video = Video.create(title: "a good title")
+    video.save
+    expect(Video.count).to eq(0)
+  end
 end
