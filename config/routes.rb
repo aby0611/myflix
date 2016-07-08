8e4d39f9fc6e67bf7770319421c0e9450536563d
Steven Huang <stevenhuang42195@gmail.com>
2016-07-08 10:04:56 +0800

    1-22 Implement the front page, register page and sign in page

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,15 @@
 Myflix::Application.routes.draw do
   get 'ui(/:action)', controller: 'ui'
 
-  get '/', to: 'videos#index'
+  root to: "pages#front"
   resources :videos, only: [:show] do
     collection do
       post :search, to: "videos#search"
     end
   end
   resources :categories, only: [:show]
+
+  get "register", to: "users#new"
+  get "sign_in", to: "sessions#new"
+  resources :users, only: [:create]
 end
