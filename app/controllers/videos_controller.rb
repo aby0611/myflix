c670e64b5a4c2f090ee1dc95a8353075c6279b8b
Steven Huang <stevenhuang42195@gmail.com>
2016-07-08 10:43:23 +0800

    1-23 Add authentication

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -2,6 +2,7 @@ class VideosController < ApplicationController
   before_filter do
     redirect_to :root if Rails.env.production?
   end
+  before_filter :require_user
 
   layout "application"
 
--- config/routes.rb
+++ config/routes.rb
@@ -11,5 +11,9 @@ Myflix::Application.routes.draw do
 
   get "register", to: "users#new"
   get "sign_in", to: "sessions#new"
+  get "sign_out", to: "sessions#destroy"
+  get "home", to: "videos#index"
+
   resources :users, only: [:create]
+  resources :sessions, only: [:create]
 end
