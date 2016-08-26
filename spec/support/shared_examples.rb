b0b287b1bf1f0e12e6527421c037e58db1a84743
Steven Huang <stevenhuang42195@gmail.com>
2016-08-26 08:34:45 +0800

    5-5 Refactor with Concerns

--- spec/support/shared_examples.rb
+++ spec/support/shared_examples.rb
@@ -5,3 +5,9 @@ shared_examples "require sign in"  do
 		expect(response).to redirect_to sign_in_path
 	end
 end
+
+shared_examples "tokenable"  do
+  it "generates a random token when the object is created" do
+    expect(object.token).to be_present
+  end
+end
